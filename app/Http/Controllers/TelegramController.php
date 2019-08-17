e17fcce12a1a1e3534dad03e153996eb5a034f65
Thabang Zwane <itmzwane@hotmail.com>
2019-08-17 23:04:17 +0200

    Debug

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -54,11 +54,16 @@ class TelegramController extends Controller
             
             if ($command == '/getBTCEquivalent') {
                 $data['default_setting'] = 'USD';
+            } else {
+                $data['default_setting'] = '';
             }
 
             if ($command == '/getBTCEquivalent' || $command == '/getUserID') {
                 $data['is_active'] = 1;
+            } else {
+                $data['is_active'] = 0;
             }
+            
             $this->sendPersonal('Pushing in '.json_encode($data));
             array_push($seed_data, $data);
             unset($data['command']); unset($data['default_setting']);
